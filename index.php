dde9529b139795ab2a989b18e939781a599ec2dd
ShroyaVardhan <110342204+ShroyaVardhan@users.noreply.github.com>
2023-07-25 11:52:03 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Web Creators</title>
+    <link rel="shortcut icon" href="logo_images/logo/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/123d276923.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+</head>
+<body>
+
+<?php include "components/header.php"  ?>
+
+
+    <section class="banner">
+        <div class="content">
+            <h3>Hey_</h3>
+            <h2>I am <span id="text"></span></h2>
+            <p>As a web developer, possess the technical expertise and creative mindset to bring websites in to life. With a deep understanding of programming languages, frameworks, and technologies, I excel in designing and building functional and visually appealing websites. My attention to detail, problem-solving skills, and ability to work collaboratively make a valuable asset in the development process. I am constantly staying updated with the latest industry trends, which play a crucial role in turning ideas into reality on the ever-evolving digital landscape.</p>
+            <a href="appointment.php">Hire Me</a>
+            <a href="port.php" class="chat">Know More</a>
+        </div>
+
+        <img src="images/home_banner.png" class="colors">
+        <img src="images/men.png" class="men">
+        <ul class="sci">
+            <li><a href="https://www.facebook.com/Iamthewebcreators" target="_blank"><ion-icon name="logo-facebook"></ion-icon></a></li>
+            <li><a href="https://www.instagram.com/iamthewebcreators/" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a></li>
+        </ul>
+
+    </section>
+
+
+    <div class="skills_main">
+
+
+        <div class="skill_heading">
+            <h1>My Skills</h1>
+            <p>Take Advantage Of The Expertise Of A Leading Online Stores Builder</p>
+        </div>
+
+
+        <div class="skill_bottom">
+
+        <div class="skill_box">
+            <img src="images/skill/html-1.svg">
+            <h2>HTML</h2>
+        </div>
+
+
+        <div class="skill_box">
+            <img src="images/skill/css-3.svg">
+            <h2>CSS</h2>
+        </div>
+
+
+        <div class="skill_box">
+            <img src="images/skill/javascript-1.svg">
+            <h2>JavaScript</h2>
+        </div>
+
+
+        <div class="skill_box">
+            <img src="images/skill/python-5.svg">
+            <h2>Python</h2>
+        </div>
+
+
+        </div>
+
+
+
+        <div class="skill_bottom">
+
+<div class="skill_box">
+    <img src="images/skill/php-1.svg">
+    <h2>PHP</h2>
+</div>
+
+
+<div class="skill_box">
+    <img src="images/skill/wordpress-icon-1.svg">
+    <h2>Wordpress</h2>
+</div>
+
+
+<div class="skill_box">
+    <img src="images/skill/sass-1.svg">
+    <h2>Scss</h2>
+</div>
+
+
+<div class="skill_box">
+    <img src="images/skill/bootstrap-5-1.svg">
+    <h2>Bootstrap</h2>
+</div>
+
+
+</div>
+</div>
+
+
+
+
+<div class="services_main">
+    <div class="service_heading">
+    <h1>Our Services</h1>
+            <p>Services Are Perfect Blend Of Creativity, Energy, Efficiency And Clarity.</p>
+    </div>
+    
+    <div class="service_bottom">
+
+        <div class="service_box">
+        <i class="fa-solid fa-desktop"></i>
+        <h1>Custom Web Development</h1>
+        <p>We have a rare blend of design and development expertise and thus, we can offer easy to operate solutions for the most complex business problems.</p>
+        </div>
+
+        <div class="service_box">
+        <i class="fa-solid fa-rectangle-ad"></i>
+        <h1>DIGITAL EXPERIENCE</h1>
+        <p>New-generation customers are more tech-savvy, and they expect a smooth and hassle-free digital experience at each step of their business transformation.</p>
+        </div>
+
+        <div class="service_box">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <h1>E-COMMERCE SOLUTION</h1>
+        <p>we along with our team of experts responsibly make sure that your online store is efficient, productive and profitable.</p>
+        </div>
+
+        <div class="service_box">
+        <i class="fa-solid fa-chart-simple"></i>
+        <h1>ONLINE BRANDING & PROMOTION</h1>
+        <p> In this competitive digital world when most of the companies are striving hard to make significant presence</p>
+        </div>
+    </div>
+
+</div> 
+
+
+
+
+
+
+<div class="home_about_main">
+    <div class="h_a_left">
+        <h2>About Us</h2>
+        <p>Hi I am a versatile professional with a passion for web development, design, social media marketing, and digital content creation. With over five years of experience in the industry, I as Shorya Vardhan Gupta have honed skills and expertise to deliver exceptional results for clients.</p>
+        <p>As a Web Developer & Designer, I possess a strong technical background and a keen eye for aesthetics. I am proficient in various programming languages and frameworks, including HTML, CSS, JavaScript, and PHP. With his deep understanding of user experience (UX) and user interface (UI) design principles, I create visually appealing and intuitive websites that engage and captivate users See My Portfolio. I am adept at crafting responsive designs that seamlessly adapt to different devices, ensuring a seamless user experience across platforms.</p>
+        <a href="#">Know More</a>
+    </div>
+
+    <div class="h_a_right">
+        <img src="images/home_about.png">
+    </div>
+
+</div>
+
+
+
+
+
+<div class="h_port_main">
+    <div class="h_p_heading">
+        <h1>Our Portfolio</h1>
+        <p>I As A IT Partner With Our Clients To Strategically Design And Develop Efficient E-Commerce Portals.</p>
+    </div>
+
+    <div class="h_p_bottom">
+
+        <a href="port/halloween" target="_blank" class="h_p_a">
+        <div class="h_p_box">
+            <img src="images/port/hallowin.png">
+            <h1>Halloween</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+        </div>
+        </a>
+
+
+        <a href="port/cars" target="_blank" class="h_p_a">
+        <div class="h_p_box">
+            <img src="images/port/cars.png">
+            <h1>Cars</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+        </div>
+        </a>
+
+        <a href="port/headphone" target="_blank" class="h_p_a">
+        <div class="h_p_box">
+            <img src="images/port/headphone.png">
+            <h1>Headphone</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+        </div>
+        </a>
+
+
+    </div>
+
+    <div class="h_p_bottom">
+
+<a href="port/plants/" target="_blank" class="h_p_a">
+<div class="h_p_box">
+    <img src="images/port/plant.png">
+    <h1>Nature</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+</div>
+</a>
+
+
+<a href="port/gym" target="_blank" class="h_p_a">
+<div class="h_p_box">
+    <img src="images/port/gym.png">
+    <h1>GYM</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+</div>
+</a>
+
+<a href="port/travel" target="_blank" class="h_p_a">
+<div class="h_p_box">
+    <img src="images/port/travel.png">
+    <h1>Tour & Travel</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio enim dolor ducimus eveniet cupiditate saepe laborum eligendi in temporibus sapiente!</p>
+</div>
+</a>
+
+
+</div>
+
+</div>
+
+
+
+
+<div class="h-contact-main" id="h_contact">
+
+    <div class="h_contact_heading">
+        <h1>Get In Touch</h1>
+    </div>
+   <form  action="https://formsubmit.co/contact@thewebcreators.in" method="POST">
+    <div class="h_contact_main">
+ 
+        <div class="h_contact_set">
+
+        <div class="h_contact_input">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Email">
+        </div>
+
+        </div>
+
+        <div class="h_contact_set">
+
+            <div class="h_contact_input">
+                <input type="date" placeholder="Date" id="date">
+                <input type="time" placeholder="Time">
+            </div>
+
+        </div>
+
+
+        <div class="h_contact_set">
+
+            <div class="h_contact_input">
+                <select name="" id="">
+                    <option value="">Select Our Service</option>
+                    <option value="Web_design">Web Development</option>
+                    <option value="DIGITAL_EXPERIENCE_SOLUTIONS">DIGITAL EXPERIENCE SOLUTIONS</option>
+                    <option value="ONLINE_BRANDING_PROMOTION">ONLINE BRANDING & PROMOTION</option>
+                    <option value="E_COMMERCE_SOLUTION">E-COMMERCE SOLUTION</option>
+                </select>
+                <input type="number" placeholder="Phone Number">
+            </div>
+
+        </div>
+
+        <button type="submit" class="submit_contact">Submit</button>
+    </div>
+    </form>
+</div>
+
+
+
+
+<?php include "components/footer.php"  ?>
+
+
+
+
+
+
+
+<script src="js/particles.js"></script>
+<script src="js/app.js"></script>
+
+
+
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+<script>
+let typingText = new Typed("#text", {
+    strings : ["Shorya", "Coder", "Web Designer", "Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 50,
+    backDelay : 1000,
+})
+
+
+
+</script>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>  
+</body>
+</html>
